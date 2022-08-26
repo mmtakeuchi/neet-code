b98f888920fccf8a3cd08a5d3a44ae666ccd7f1e
mmtakeuchi <mikemtaki@gmail.com>
2022-08-26 08:58:23 -0700

    valid palindrome

---
+++ Two Pointers/valid-palindrome.js
@@ -0,0 +1,20 @@
+var isPalindrome = function (s) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const newArr = s
+    .split('')
+    .filter((char) => alphabet.includes(char.toLowerCase()));
+
+  let left = 0;
+  let right = newArr.length - 1;
+
+  while (left <= right) {
+    if (newArr[left].toLowerCase() !== newArr[right].toLowerCase()) {
+      return false;
+    }
+
+    left += 1;
+    right -= 1;
+  }
+
+  return true;
+};
