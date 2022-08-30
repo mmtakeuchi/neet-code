ead846a96f6b9af76d1d3a94f86c8626e8fe3d9a
mmtakeuchi <mikemtaki@gmail.com>
2022-08-30 09:28:19 -0700

    reverse linked list

---
+++ Linked List/reverse-linked-list.js
@@ -0,0 +1,30 @@
+// Iteratively
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
+
+// Recursively
+var reverseList = function (head) {
+  if (!head) return null;
+
+  let newHead = head;
+
+  if (head.next) {
+    newHead = reverseList(head.next);
+    head.next.next = head;
+  }
+
+  head.next = null;
+
+  return newHead;
+};
