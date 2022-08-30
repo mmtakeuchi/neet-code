8b84751b7820cb86a454bdf96e3a6bfb78634cad
mmtakeuchi <mikemtaki@gmail.com>
2022-08-30 10:19:32 -0700

    merge two sorted lists

---
+++ Linked List/merge-two-sorted-lists.js
@@ -0,0 +1,20 @@
+var mergeTwoLists = function (list1, list2) {
+  let list = new ListNode(null);
+  let node = list;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      node.next = list1;
+      list1 = list1.next;
+    } else {
+      node.next = list2;
+      list2 = list2.next;
+    }
+
+    node = node.next;
+  }
+
+  node.next = list1 || list2;
+
+  return list.next;
+};
