e5bf653ca2c300a039e7283865079af2ef6499f4
mmtakeuchi <mikemtaki@gmail.com>
2022-09-07 14:43:19 -0700

    lowest common ancestor of a binary search tree

---
+++ Trees/lowest-common-ancestor-of-a-bst.js
@@ -0,0 +1,14 @@
+var lowestCommonAncestor = function (root, p, q) {
+  while (root) {
+    if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else {
+      return root;
+    }
+  }
+};
+
+// Time: O(H), where H is the heigh of Binary Tree.
+// Space: O(1)
