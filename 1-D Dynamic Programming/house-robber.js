8925af77ede3b5c22b4c50af9882bb46200d907e
mmtakeuchi <mikemtaki@gmail.com>
2022-09-10 18:53:48 -0700

    house robber

---
+++ 1-D Dynamic Programming/house-robber.js
@@ -0,0 +1,15 @@
+var rob = function (nums) {
+  let rob1 = 0;
+  let rob2 = 0;
+
+  for (const n of nums) {
+    let temp = Math.max(n + rob1, rob2);
+    rob1 = rob2;
+    rob2 = temp;
+  }
+
+  return rob2;
+};
+
+// Time: O(n)
+// Space: O(1)
