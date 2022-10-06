93949e5e4afd368f66ff54f56ed8abe8206dbb08
mmtakeuchi <mikemtaki@gmail.com>
2022-10-05 21:27:42 -0700

    permutations, backtracking

---
+++ Backtracking/permutations.js
@@ -0,0 +1,27 @@
+var permute = function (nums) {
+  return dfs(nums);
+};
+
+var dfs = function (nums, permutation = [], permutations = []) {
+  const isBaseCase = nums.length === permutation.length;
+  if (isBaseCase) return permutations.push([...permutation]);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (permutation.includes(nums[i])) continue;
+
+    backTrack(nums, i, permutation, permutations);
+  }
+
+  return permutations;
+};
+
+const backTrack = (nums, i, permutation, permutations) => {
+  permutation.push(nums[i]);
+
+  dfs(nums, permutation, permutations);
+
+  permutation.pop();
+};
+
+// Time: O(N!)
+// Space: O(N!)
