c858b45695650c8a05662ab889e541bfcf2ba5f3
mmtakeuchi <mikemtaki@gmail.com>
2022-10-06 11:46:04 -0700

    add recursive lowest common ancestor, tree

--- Trees/lowest-common-ancestor-of-a-bst.js
+++ Trees/lowest-common-ancestor-of-a-bst.js
@@ -10,5 +10,16 @@ var lowestCommonAncestor = function (root, p, q) {
   }
 };
 
+var lowestCommonAncestorRecur = function (root, p, q) {
+  if (!root) return null;
+  if (root === p || root === q) return root;
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+
+  if (left && right) return root;
+  return left || right;
+};
+
 // Time: O(H), where H is the heigh of Binary Tree.
 // Space: O(1)
