98788693018d49ce42bbf892e162ec0073fc050e
mmtakeuchi <mikemtaki@gmail.com>
2022-10-18 13:16:57 -0700

    search insert position, binary search

---
+++ Binary Search/search-insert-position.js
@@ -0,0 +1,21 @@
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};
+
+// Time: O(logn)
+// Space: O(1)
